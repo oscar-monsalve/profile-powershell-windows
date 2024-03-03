1a6aa0cda0cd2b12fc4db4956a670259a43a034b
oscar-monsalve <oscarmonsalvec5@gmail.com>
2024-03-03 18:35:14 -0500

    added 'vim' alias

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,17 @@
+# Delete initial PowerShell initial messages
 Clear-Host
-oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/robbyrussell.omp.json' | Invoke-Expression
 
+# Set Oh-My-Posh font
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/robbyrussell.omp.json' | Invoke-Expression
 
 # fzf
 $env:FZF_DEFAULT_COMMAND='fd --type d --hidden --follow --exclude .git'
 
 $env:FZF_DEFAULT_OPTS="--border"
 
-
-function fd{
+function fd {
     cd $(fzf)
 }
+
+# Set aliases
+Set-Alias vim nvim
