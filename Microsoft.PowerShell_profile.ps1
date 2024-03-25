60d4ea070f5cce88d89fa05f214d9f88779c6e28
oscar-monsalve <oscarmonsalvec5@gmail.com>
2024-03-25 15:10:32 -0500

    added keyboard shortcuts

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,9 @@
 # Delete initial PowerShell initial messages
 Clear-Host
 
+# Setting UTF8 encoding
+$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+
 # Set Oh-My-Posh font
 oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/robbyrussell.omp.json' | Invoke-Expression
 
@@ -9,9 +12,38 @@ $env:FZF_DEFAULT_COMMAND='fd --type d --hidden --follow --exclude .git'
 
 $env:FZF_DEFAULT_OPTS="--border"
 
+# Custom functions
 function fd {
     cd $(fzf)
 }
 
+function start_powershell {
+    start powershell
+}
+
+function onedrive {
+    # Change to the D: drive
+    Set-Location D:\
+
+    # Specify the target path with encoded "ó" character
+    $encodedPath = [System.Text.Encoding]::UTF8.GetString([System.Text.Encoding]::Default.GetBytes(".\OneDrive - Instituto Tecnológico Metropolitano\"))
+
+    # Check if the path already exists
+    if (Test-Path $encodedPath) {
+        # Change to the target path on the D: drive
+        Set-Location $encodedPath
+    } else {
+        Write-Warning "The specified path does not exist: $encodedPath"
+    }
+}
+
+# Keyboard shortcuts
+Set-PSReadLineKeyHandler -Key "Ctrl+P" -ScriptBlock {start_powershell}
+Set-PSReadLineKeyHandler -Key "Ctrl+F" -ScriptBlock {fd}
+# Change to D: drive and cd into the onedrive directory
+Set-PSReadLineKeyHandler -Key "Ctrl+O" -ScriptBlock {onedrive}
+
+
 # Set aliases
 Set-Alias vim nvim
+Set-Alias arduino "~/AppData/Local/Programs/Arduino IDE/Arduino IDE.exe"
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # $PROFILE
 
-- Configuration file for the PowerShell terminal on Windows.
-- Path is "~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1".
+Configuration file for the PowerShell terminal on Windows.
+
+Its path is "~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1".
